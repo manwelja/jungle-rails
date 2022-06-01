b33a013fce25f0b6396de3636eae967b75b38495
Jennifer Manwell <manwell.fortier@gmail.com>
2022-06-01 19:10:40 -0400

    Fix bug in About Us link

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,42 @@
 class Admin::CategoriesController < ApplicationController
+    #implement basic user authentication with credentials specified in .env
+    Rails.configuration.authenticate = {
+      :username_key => ENV['AUTH_USERNAME_KEY'],
+      :password_key => ENV['AUTH_PASSWORD_KEY']
+    }
+    
+    http_basic_authenticate_with name: Rails.configuration.authenticate[:username_key], password: Rails.configuration.authenticate[:password_key]
+    protect_from_forgery with: :exception
+  
+    def index
+      @products = Product.order(id: :desc).all
+    end
+  
+    def new
+      @product = Product.new
+    end
+  
+    def create
+      @product = Product.new(product_params)
+  
+      if @product.save
+        redirect_to [:admin, :products], notice: 'Product created!'
+      else
+        render :new
+      end
+    end
+  
+    private
+  
+    def product_params
+      params.require(:product).permit(
+        :name,
+        :description,
+        :category_id,
+        :quantity,
+        :image,
+        :price
+      )
+    end
+  
 end
