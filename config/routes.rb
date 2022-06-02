eb94caf5f800b17655066f1b3e5d045a0e5dc93a
Jennifer Manwell <manwell.fortier@gmail.com>
2022-06-02 11:30:21 -0400

    Rename session to sessions

--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,15 @@ Rails.application.routes.draw do
   end
 
   # Routes used for user authentication
+#  resources :users, only: [:new, :create]  
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+
+  #Routes used for user sign in
+  #resources :sessions, only: [:new, :create, :destroy]  
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
