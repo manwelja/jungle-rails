0c986063506f09eab67636cc94ef850a7e2fdfb9
Jennifer Manwell <manwell.fortier@gmail.com>
2022-06-02 11:11:08 -0400

    Add user db migration and controller

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,10 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
+  # Routes used for user authentication
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
