5661c82e7647cc00509f69a12feea9b64dce7b7f
Jennifer Manwell <manwell.fortier@gmail.com>
2022-06-02 11:50:13 -0400

    Implement user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,8 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    if(user.save
-      session[:user_id] = user.user_id
+    if(user.save)
+      session[:user_id] = user.id
       redirect to '/'
     else
       redirect_to '/signup'
